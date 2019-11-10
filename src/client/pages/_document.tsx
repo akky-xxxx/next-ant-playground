4ca220885e330a290377ff564d1ac51b69c2a959
Shogo Akimoto <114.s.aki@gmail.com>
2019-11-10 16:17:25 +0900

    feat: implement _doc

---
+++ src/client/pages/_document.tsx
@@ -0,0 +1,45 @@
+import React, { Fragment } from "react"
+import Document, { Head, Main, NextScript, DocumentContext } from "next/document"
+import { ServerStyleSheet } from "styled-components"
+import { clone } from "remeda"
+
+export default class MyDocument extends Document {
+  static async getInitialProps(context: DocumentContext) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = clone(context.renderPage)
+    const newContext = clone(context)
+    try {
+      newContext.renderPage = () =>
+        originalRenderPage({
+          // eslint-disable-next-line react/jsx-props-no-spreading
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+        })
+      const initialProps = await Document.getInitialProps(newContext)
+      return {
+        ...initialProps,
+        styles: (
+          <Fragment>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </Fragment>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
+
+  render(): JSX.Element {
+    return (
+      <html lang="ja">
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0" key="viewport" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
