7b93fdc350278109fc674f2b4b0f9bf36120ae85
Shogo Akimoto <114.s.aki@gmail.com>
2019-11-17 16:02:57 +0900

    入力業実装中

---
+++ src/client/components/molecules/fieldsRow/index.tsx
@@ -0,0 +1,130 @@
+/**
+ * import node_modules
+ */
+import React, { FC } from "react"
+import { Form, Row, Input, Popover, Button } from "antd"
+import styled from "styled-components"
+
+/**
+ * import others
+ */
+import STYLE_VARIABLES from "../../../assets/styles/variables"
+import ErrorMessage from "../../atoms/errorMessage"
+
+/**
+ * main
+ */
+interface FieldsRowProps {
+  index: number
+  fields: {
+    isValid: boolean
+    handleRemoveField: Function
+    handleAddField: Function
+    rowId: string
+    input1: {
+      inputValue: number
+      handleChangeValue: Function
+      errorMessage: null | string
+      isInput: boolean
+      isValid: boolean
+      id: string
+    }
+    input2: {
+      inputValue: number
+      handleChangeValue: Function
+      errorMessage: null | string
+      isInput: boolean
+      isValid: boolean
+      id: string
+    }
+  }
+}
+
+const FieldsRow: FC<FieldsRowProps> = props => {
+  const {
+    index,
+    fields: { input1, input2, isValid, handleAddField, handleRemoveField, rowId },
+  } = props
+
+  return (
+    <StyledRow label={`条件 ${index + 1}`} colon={false}>
+      <Row type="flex" justify="space-between">
+        <Row type="flex">
+          <div>
+            <Popover
+              content={<ErrorMessage message={input1.errorMessage} />}
+              visible={input1.isInput && !input1.isValid}
+              placement="bottomLeft"
+            >
+              <Input
+                value={input1.inputValue || ""}
+                onChange={event =>
+                  input1.handleChangeValue({
+                    targetId: rowId,
+                    newValue: event.target.value,
+                    isRequire: index === 0,
+                  })
+                }
+              />
+            </Popover>
+          </div>
+
+          <Connect>が</Connect>
+
+          <div>
+            <Popover
+              content={<ErrorMessage message={input2.errorMessage} />}
+              visible={input2.isInput && !input2.isValid}
+              placement="bottomLeft"
+            >
+              <Input
+                value={input2.inputValue || ""}
+                onChange={event =>
+                  input2.handleChangeValue({
+                    targetId: rowId,
+                    newValue: event.target.value,
+                    isRequire: index === 0,
+                  })
+                }
+              />
+            </Popover>
+          </div>
+        </Row>
+
+        <div>
+          <Row type="flex" justify="space-between">
+            {index !== 0 ? (
+              <MinusButton shape="circle" onClick={() => handleRemoveField({ targetId: rowId })}>
+                -
+              </MinusButton>
+            ) : (
+              <span />
+            )}
+            <Button shape="circle" onClick={() => handleAddField({ targetId: rowId })} disabled={!isValid}>
+              +
+            </Button>
+          </Row>
+        </div>
+      </Row>
+    </StyledRow>
+  )
+}
+
+const StyledRow = styled(Form.Item)`
+  margin-bottom: 0;
+
+  :not(:first-child) {
+    margin-top: ${STYLE_VARIABLES.MARGINS.MARGIN12}px;
+  }
+`
+
+const Connect = styled.span`
+  margin-left: ${STYLE_VARIABLES.MARGINS.MARGIN8}px;
+  margin-right: ${STYLE_VARIABLES.MARGINS.MARGIN8}px;
+`
+
+const MinusButton = styled(Button)`
+  margin-right: 8px;
+`
+
+export default FieldsRow
