23483383bcac70a68a515a456c74b5f7e4aa88fc
Shogo Akimoto <114.s.aki@gmail.com>
2020-03-21 15:55:05 +0900

    feat(bff): divide token refresh main stream
    https://github.com/akky-xxxx/next-ant-playground/issues/39

--- src/server/middlewares/oauth/handlers/tokenRefreshHandler.ts
+++ src/server/middlewares/oauth/handlers/tokenRefreshHandler.ts
@@ -8,15 +8,12 @@ import { RequestHandler } from "express"
  */
 import createLogger from "../../../utils/createLogger"
 import isPageRequest from "../../../utils/isPageRequest"
-// import str2mask from "../../../utils/str2mask"
-import { ExtendedRequest } from "../../../types/common"
-import getNewToken from "../modules/getNewToken"
-import getTokenExpire from "../modules/getTokenInfo"
+import updateToken from "../../../utils/updateToken"
 
 /**
  * main
  */
-const { sillyLogger, infoLogger, errorLogger } = createLogger("tokenRefreshHandler")
+const { sillyLogger, errorLogger } = createLogger("tokenRefreshHandler")
 
 const tokenRefreshHandler: RequestHandler = async (req, _res, next) => {
   if (!isPageRequest(req.url)) {
@@ -26,71 +23,10 @@ const tokenRefreshHandler: RequestHandler = async (req, _res, next) => {
 
   sillyLogger("tokenRefreshHandler 開始")
 
-  const { user, session } = req as ExtendedRequest
-
-  if (!session) {
-    sillyLogger("session がありません")
-    next()
-    return
-  }
-
-  if (!user) {
-    sillyLogger("user がありません")
-    next()
-    return
-  }
-
-  const { displayName, email, expire, refreshToken } = user
-
-  if (!refreshToken) {
-    sillyLogger("refreshToken がありません")
-    next()
-    return
-  }
-
-  if (expire && new Date(expire) >= new Date()) {
-    infoLogger({
-      message: "access token は有効です",
-      data: {
-        email,
-        expire,
-      },
-    })
-    next()
-    return
-  }
-
-  infoLogger({
-    message: "access token が無効です",
-    data: {
-      email,
-      expire,
-    },
-  })
-  // token の発行と有効期限の取得
   try {
-    const newToken = await getNewToken(refreshToken)
-    const { access_token } = newToken
-
-    const tokenInfo = await getTokenExpire(access_token)
-    const { exp } = tokenInfo
-
-    if (req.session) {
-      req.session.passport.user = {
-        ...user,
-        accessToken: access_token,
-        expire: exp,
-      }
-    }
-
-    infoLogger({
-      message: "token refresh 完了",
-      data: { user: { displayName, email, expire } },
-    })
-    next()
+    await updateToken(req, next)
   } catch (error) {
     errorLogger({ error })
-    next()
   }
 }
 
---
+++ src/server/utils/updateToken.ts
@@ -0,0 +1,98 @@
+/**
+ * import node_modules
+ */
+import { Request, NextFunction } from "express"
+
+/**
+ * import others
+ */
+import { ExtendedRequest } from "../types/common"
+import getNewToken from "../middlewares/oauth/modules/getNewToken"
+import getTokenExpire from "../middlewares/oauth/modules/getTokenInfo"
+import createLogger from "./createLogger"
+
+/**
+ * main
+ */
+type UpdateToken = (req: Request, next?: NextFunction) => Promise<void>
+
+const { sillyLogger, infoLogger, errorLogger } = createLogger("updateToken")
+
+const updateToken: UpdateToken = async (req, next) => {
+  const { user, session } = req as ExtendedRequest
+
+  if (!session) {
+    sillyLogger("session がありません")
+
+    if (next) next()
+    return Promise.resolve()
+  }
+
+  if (!user) {
+    sillyLogger("user がありません")
+
+    if (next) next()
+    return Promise.resolve()
+  }
+
+  const { displayName, email, expire, refreshToken } = user
+
+  if (!refreshToken) {
+    sillyLogger("refreshToken がありません")
+
+    if (next) next()
+    return Promise.resolve()
+  }
+
+  if (expire && new Date(expire) >= new Date()) {
+    infoLogger({
+      message: "access token は有効です",
+      data: {
+        email,
+        expire,
+      },
+    })
+
+    if (next) next()
+    return Promise.resolve()
+  }
+
+  infoLogger({
+    message: "access token が無効です",
+    data: {
+      email,
+      expire,
+    },
+  })
+  // token の発行と有効期限の取得
+  try {
+    const newToken = await getNewToken(refreshToken)
+    const { access_token } = newToken
+
+    const tokenInfo = await getTokenExpire(access_token)
+    const { exp } = tokenInfo
+
+    if (req.session) {
+      req.session.passport.user = {
+        ...user,
+        accessToken: access_token,
+        expire: exp,
+      }
+    }
+
+    infoLogger({
+      message: "token refresh 完了",
+      data: { user: { displayName, email, expire } },
+    })
+
+    if (next) next()
+    return Promise.resolve()
+  } catch (error) {
+    errorLogger({ error })
+
+    if (next) next()
+    return Promise.reject()
+  }
+}
+
+export default updateToken
