7721ea7d6aca47b8ca3767255e1d2e43aaedab7c
Shogo Akimoto <114.s.aki@gmail.com>
2020-03-22 14:57:40 +0900

    refactor(client): organize structure of antTable

--- src/client/pages/ant-table/index.tsx
+++ src/client/pages/ant-table/index.tsx
@@ -1,41 +1,29 @@
-import React, { Fragment } from "react"
-import { Table } from "antd"
-import { PaginationConfig } from "antd/es/pagination"
-import { ColumnProps } from "antd/es/table"
+/**
+ * import node_modules
+ */
+import { connect } from "react-redux"
 
-interface Data {
-  key: number
-  name: string
-  sex: "1" | "2"
+/**
+ * import others
+ */
+import { pageNameMap } from "../../shared/const/common"
+import { InitialState, actions } from "../../store/modules"
+
+import AntTable, { HandleActions } from "../../components/pages/antTable"
+
+const {
+  app: { checkToken },
+} = actions
+
+AntTable.getInitialProps = async () => {
+  return {
+    currentPage: pageNameMap.antTable,
+  }
 }
 
-const data: Data[] = [...new Array(100)].fill("").map((_, index) => ({
-  key: index,
-  name: `住民 ${index + 1}`,
-  sex: index % 2 ? "1" : "2",
-}))
-
-const columns: ColumnProps<Data>[] = [
-  {
-    title: "Name",
-    dataIndex: "name",
-    key: "name",
-  },
-  {
-    title: "Sex",
-    dataIndex: "sex",
-    key: "sex",
-    render: value => <Fragment>{value === "1" ? "男" : "女"}</Fragment>,
-  },
-]
-
-const paginationOption: PaginationConfig = {
-  defaultPageSize: 20,
-  position: "both",
-}
-
-const TableWindow = () => {
-  return <Table columns={columns} dataSource={data} pagination={paginationOption} />
-}
-
-export default TableWindow
+export default connect<{}, HandleActions, {}, InitialState>(
+  () => ({}),
+  () => dispatch => ({
+    handleCheckToken: () => dispatch(checkToken()),
+  }),
+)(AntTable)
