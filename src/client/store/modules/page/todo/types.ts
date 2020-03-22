5532434c65f9f50431dd784ada55e57e28503d87
Shogo Akimoto <114.s.aki@gmail.com>
2020-03-22 22:59:28 +0900

    feat(client): connect agreed and store

--- src/client/store/modules/page/todo/types.ts
+++ src/client/store/modules/page/todo/types.ts
@@ -6,10 +6,22 @@ import { Action } from "redux-actions"
 /**
  * main
  */
+export interface ToDoItem {
+  id: string
+  title: string
+  detail: string
+  isDone: boolean
+}
+
 export interface InitialState {
   master: {
     isLoading: boolean
+    todoList: ToDoItem[]
   }
+}
+
+export interface SuccessAction {
+  payload: ToDoItem[]
 }
 
 export interface HandleActions {
