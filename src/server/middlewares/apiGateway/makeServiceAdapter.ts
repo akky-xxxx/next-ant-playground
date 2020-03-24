19bb7c99f15fa2cce233dd2b387962dffa98ca2b
Shogo Akimoto <114.s.aki@gmail.com>
2020-03-25 00:21:26 +0900

    refactor(bff): organize types

--- src/server/middlewares/apiGateway/makeServiceAdapter.ts
+++ src/server/middlewares/apiGateway/makeServiceAdapter.ts
@@ -1,10 +1,5 @@
 /**
- * import node_modules
- */
-import { Request } from "express"
-
-/**
- * import others
+ * import
  */
 import { BaseService, CreateMethod, DeleteMethod, ReadMethod, UpdateMethod } from "../../services/BaseService"
 
@@ -19,22 +14,18 @@ type Adapter = {
   update?: UpdateMethod
 }
 
+type MakeServiceAdapter = (service: BaseService) => Adapter
+
 const methods1 = ["read", "delete"] as const
 const methods2 = ["create", "update"] as const
 
-function makeServiceAdapter(service: BaseService) {
+const makeServiceAdapter: MakeServiceAdapter = service => {
   const adapter: Adapter = {
     name: service.name,
   }
   // read と delete
   methods1.forEach(method => {
-    adapter[method] = async (
-      req: Request,
-      resource: string,
-      params: Record<string, unknown>,
-      config: unknown,
-      callback: Function,
-    ) => {
+    adapter[method] = async (req, resource, params, config, callback) => {
       service[method](req, resource, params, config, callback).then(
         result => {
           callback(null, result)
@@ -48,14 +39,7 @@ function makeServiceAdapter(service: BaseService) {
 
   // create と update
   methods2.forEach(method => {
-    adapter[method] = async (
-      req: Request,
-      resource: string,
-      params: Record<string, unknown>,
-      body: Record<string, unknown>,
-      config: unknown,
-      callback: Function,
-    ) => {
+    adapter[method] = async (req, resource, params, body, config, callback) => {
       service[method](req, resource, params, body, config, callback).then(
         result => {
           callback(null, result)
