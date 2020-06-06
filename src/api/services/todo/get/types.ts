6233d5e16fbe190d302876eac57902ca59a1af66
Shogo Akimoto <114.s.aki@gmail.com>
2020-06-06 14:05:28 +0900

    refactor(api): resolve ts error

--- src/api/services/todo/get/types.ts
+++ src/api/services/todo/get/types.ts
@@ -17,14 +17,15 @@ export interface ToDoItem {
 }
 
 type ResponseBodyType = ToDoItem[]
+export type AnyObject = Record<string, unknown>
 
 export type ApiType = AgreedUi &
   APIDef<
     GET, // HTTP Method
     ["api", "todo", "get"], // API Path
-    {}, // request header
-    {}, // request query
+    AnyObject, // request header
+    AnyObject, // request query
     undefined, // request body
-    {}, // response header
+    AnyObject, // response header
     ResponseDef<Success200, ResponseBodyType> | ResponseDef<Error400, { errorMessage: "error message" }> // response body
   >
